47d0d0d2dfe9bb00af28dc5c31a0f2e9811dc1eb
kchnr <jean.kirchner@gmail.com>
2022-12-18 20:23:02 -0300

    add copilot to neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -58,6 +58,9 @@ require('packer').startup(function(use)
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
 
+  -- Github Copilot
+  use 'github/copilot.vim'
+
   -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
   local has_plugins, plugins = pcall(require, 'custom.plugins')
   if has_plugins then

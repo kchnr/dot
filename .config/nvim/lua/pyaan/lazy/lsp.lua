c996ab2c480d22e6f2a952994837787b0300fae1
kchnr <jean.kirchner@gmail.com>
2025-08-10 17:02:02 -0300

    add hover shortcut

--- .config/nvim/lua/pyaan/lazy/lsp.lua
+++ .config/nvim/lua/pyaan/lazy/lsp.lua
@@ -72,6 +72,9 @@ return {
                         vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
                     end
 
+                    -- Hover to see type information and docs
+                    map("K", vim.lsp.buf.hover, "Hover Documentation")
+
                     -- Jump to the definition of the word under your cursor.
                     --  This is where a variable was first declared, or where a function is defined, etc.
                     --  To jump back, press <C-t>.

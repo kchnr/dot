a0f87a8484718c577592ad812b2a3509e8406769
kchnr <jean.kirchner@gmail.com>
2025-08-10 14:53:34 -0300

    fix vim color issue

--- .config/nvim/lua/pyaan/lazy/color.lua
+++ .config/nvim/lua/pyaan/lazy/color.lua
@@ -1,67 +1,23 @@
-function ColorMyPencils(color)
-    color = color or "rose-pine"
-    vim.cmd.colorscheme(color)
-
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+local function ColorMyPencils(scheme)
+    scheme = scheme or "tokyonight" -- pick your default here
+    vim.cmd.colorscheme(scheme)
 end
 
 return {
+    -- Keep the themes you like installed…
+    { "folke/tokyonight.nvim", priority = 1000 }, -- load early
+    { "ellisonleao/gruvbox.nvim", name = "gruvbox" },
+    { "rose-pine/neovim", name = "rose-pine" },
+    { "fugalh/desert.vim" },
+    { "erikbackman/brightburn.vim" },
 
+    -- Then, after plugins load, apply ONE scheme:
     {
-        "erikbackman/brightburn.vim",
-    },
-
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        opts = {},
+        "nvim-lua/plenary.nvim", -- any always-loaded plugin works as a hook point
         config = function()
-            ColorMyPencils()
-        end,
-    },
-    {
-        "ellisonleao/gruvbox.nvim",
-        name = "gruvbox",
-        config = function()
-            require("gruvbox").setup({
-                terminal_colors = true, -- add neovim terminal colors
-                undercurl = true,
-                underline = false,
-                bold = true,
-                italic = {
-                    strings = false,
-                    emphasis = false,
-                    comments = false,
-                    operators = false,
-                    folds = false,
-                },
-                strikethrough = true,
-                invert_selection = false,
-                invert_signs = false,
-                invert_tabline = false,
-                invert_intend_guides = false,
-                inverse = true, -- invert background for search, diffs, statuslines and errors
-                contrast = "", -- can be "hard", "soft" or empty string
-                palette_overrides = {},
-                overrides = {},
-                dim_inactive = false,
-                transparent_mode = false,
-            })
-        end,
-    },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require("rose-pine").setup({
-                disable_background = true,
-                styles = {
-                    italic = false,
-                },
-            })
-
-            ColorMyPencils()
+            vim.opt.termguicolors = true
+            vim.opt.background = "dark"
+            ColorMyPencils("tokyonight") -- or "gruvbox" / "rose-pine" / "desert"
         end,
     },
 }

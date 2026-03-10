a5ef66c2037d5ac5f2c8211796049e1e4ebb3f30
kchnr <jean.kirchner@gmail.com>
2026-03-10 20:48:49 -0300

    improve kitty / neovim themes and add vscode configs to dotfiles

--- .config/nvim/lua/pyaan/lazy/color.lua
+++ .config/nvim/lua/pyaan/lazy/color.lua
@@ -1,23 +1,27 @@
 local function ColorMyPencils(scheme)
-    scheme = scheme or "tokyonight" -- pick your default here
+    scheme = scheme or "leaf"
     vim.cmd.colorscheme(scheme)
 end
 
 return {
     -- Keep the themes you like installed…
-    { "folke/tokyonight.nvim", priority = 1000 }, -- load early
+    { "folke/tokyonight.nvim" },
+    { "catppuccin/nvim" },
+    { "altercation/vim-colors-solarized" },
     { "ellisonleao/gruvbox.nvim", name = "gruvbox" },
     { "rose-pine/neovim", name = "rose-pine" },
     { "fugalh/desert.vim" },
-    { "erikbackman/brightburn.vim" },
+    { "sainnhe/everforest" },
+    { "rebelot/kanagawa.nvim" },
+    { "daschw/leaf.nvim" },
 
     -- Then, after plugins load, apply ONE scheme:
     {
         "nvim-lua/plenary.nvim", -- any always-loaded plugin works as a hook point
         config = function()
             vim.opt.termguicolors = true
-            vim.opt.background = "dark"
-            ColorMyPencils("tokyonight") -- or "gruvbox" / "rose-pine" / "desert"
+            vim.opt.background = "light"
+            ColorMyPencils("everforest")
         end,
     },
 }
--- .config/nvim/lua/pyaan/lazy/telescope.lua
+++ .config/nvim/lua/pyaan/lazy/telescope.lua
@@ -1,32 +1,24 @@
 return {
     "nvim-telescope/telescope.nvim",
-
-    tag = "0.1.5",
-
     dependencies = {
         "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
-
     config = function()
-        require("telescope").setup({})
-
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-        vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[F]iles [G]rep" })
-        vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 
-        vim.keymap.set("n", "<leader>pws", function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set("n", "<leader>pWs", function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set("n", "<leader>ps", function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+        require("telescope").setup({
+            defaults = {
+                path_display = { "smart" },
+            },
+            extensions = {
+                fzf = {}, -- Uses the C-sorter for speed
+            },
+        })
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope [F]ind [F]iles" })
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope [F]ile [G]rep" })
+        vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope [F]ind [B]uffers" })
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope [F]ind [H]elp tags" })
+        vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope git files" })
     end,
 }
